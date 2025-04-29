88b4fd3e3b045512adb17cf9577da09256108543
glaucia-santos <glauaasantos@gmail.com>
2025-04-29 03:08:06 -0300

    Corrigindo vite.config.js para build na Vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  root: '.', // garante que a raiz seja a pasta atual
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: path.resolve(__dirname, 'public/index.html'),
+    },
+  },
+  server: {
+    port: 3000,
+  },
 })
-
